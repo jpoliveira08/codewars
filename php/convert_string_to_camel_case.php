fdad105487ff026c88862b9a330811b9d1902b9b
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-11-13 23:53:43 -0300

    Add Convert string to camel case

---
+++ php/convert_string_to_camel_case.php
@@ -0,0 +1,16 @@
+<?php
+
+function toCamelCase($str){
+    $newStr = str_split($str);
+
+    for ($i=0; $i < strlen($str); $i++) { 
+        if($newStr[$i] == "-" || $newStr[$i] == "_"){
+            unset($newStr[$i]);
+            $newStr = array_values($newStr);
+            $newStr[$i] = strtoupper($newStr[$i]);
+        }
+    }
+    return implode($newStr);
+}
+
+echo toCamelCase("the-stealth-warrior");
