42d63dfc9f326500180ba7c6c5c1acbf81ce7550
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-11-20 20:39:37 -0300

    Count the divisors of a number

---
+++ php/count_the_divisors_of_a_number.php
@@ -0,0 +1,13 @@
+<?php
+
+function divisors($n) {
+    $count = 0;
+    for ($i=1; $i <= $n; $i++) { 
+        if($n % $i == 0){
+            $count++;
+        }
+    }
+    return $count;
+  }
+
+echo divisors(30);
