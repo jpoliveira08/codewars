9173483c549fd36b271a202d3b101cee8fa47b2b
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-11-23 20:08:11 -0300

    Correcting the code

--- php/sum_of_odd_numbers.php
+++ php/sum_of_odd_numbers.php
@@ -7,7 +7,6 @@ function rowSumOddNumbers($n)
     $num = 1;
     for ($row=0; $row < $n; $row++) { 
         for ($col=0; $col <= $row; $col++) { 
-            echo "linha: {$row} coluna: {$col} = {$num}" . PHP_EOL;
             $teste[$row][$col] = $num;
             $num += 2;
             if($row == ($n - 1)){
@@ -15,9 +14,5 @@ function rowSumOddNumbers($n)
             }
         }
     }
-    var_dump($teste);
     return $sum;
 }
-
-echo PHP_EOL;
-echo rowSumOddNumbers(13);
