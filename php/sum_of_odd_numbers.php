cc1a8687f6d8a79402f63504dbd321ff96941104
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-11-23 20:03:30 -0300

    Sum Of Odd Numbers Finalized!

--- php/sum_of_odd_numbers.php
+++ php/sum_of_odd_numbers.php
@@ -4,33 +4,20 @@ function rowSumOddNumbers($n)
 {
     $sum = 0;
     $teste = array(array());
-    $num = 0;
-    /**
-     * Será a partir do n que saberemos a quantidade de linhas que esta matriz contem
-     * A cada nova linha uma nova coluna sera adicionada
-     * Os elementos serao ordenados da esqiuerda para direita
-     */
+    $num = 1;
     for ($row=0; $row < $n; $row++) { 
-        for ($col=0; $col <= $n; $col++) { 
-            if(oddNumber($num)){
-                $teste[$row][$col] = $num;
-                if($row == ($n - 1)){
-                    $sum += $teste[$row][$col];
-                }
-                echo "linha: {$row} coluna: {$col} = {$num}" . PHP_EOL;
+        for ($col=0; $col <= $row; $col++) { 
+            echo "linha: {$row} coluna: {$col} = {$num}" . PHP_EOL;
+            $teste[$row][$col] = $num;
+            $num += 2;
+            if($row == ($n - 1)){
+                $sum += $teste[$row][$col];
             }
-            $num++;
         }
     }
     var_dump($teste);
     return $sum;
 }
-function oddNumber($number) : bool
-{
-    if($number % 2 !== 0){
-        return true;
-    }
-    return false;
-}
+
 echo PHP_EOL;
-echo rowSumOddNumbers(2);
+echo rowSumOddNumbers(13);
