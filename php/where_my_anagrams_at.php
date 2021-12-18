2305f902ce7938006edcba86a3a6aab53753139b
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-12-18 11:52:15 -0300

    Correcting the Where my anagrams At?

--- php/where_my_anagrams_at.php
+++ php/where_my_anagrams_at.php
@@ -1,19 +1,22 @@
 <?php
 
 function anagrams(string $word, array $words): array {
+
     $anagramsWords = []; 
     $arraySameSize = [];
+
     foreach($words as $oneWord){
         if(strlen($oneWord) == strlen($word)) array_push($arraySameSize, $oneWord);
     }
     for ($i=0; $i < count($arraySameSize); $i++) { 
         $cont = 0;
-        for ($j=0; $j < strlen($word); $j++) { 
+        for ($j=0; $j < strlen($arraySameSize[$i]); $j++) { 
             if(str_contains($arraySameSize[$i], $word[$j]) && str_contains($word, $arraySameSize[$i][$j])) $cont++;
         }
         if($cont == strlen($word)){
             array_push($anagramsWords, $arraySameSize[$i]);
         }
     }
+
     return $anagramsWords;
 }
