e6e694a3a48fa945d130d1dae6b93b8667ac465a
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2022-02-15 20:22:47 -0300

    ConvertNumberToReversedArrayOfDigits Fixed

--- kotlin/src/main/kotlin/ConvertNumberToReversedArrayOfDigits.kt
+++ kotlin/src/main/kotlin/ConvertNumberToReversedArrayOfDigits.kt
@@ -1,21 +1,8 @@
-fun main() {
-    var numero: Long = 789654;
-    var numeroEmTexto = numero.toString().toCharArray()
-    var teste = (numeroEmTexto.size - 1)
-    var essevai = IntArray(numeroEmTexto.size)
-    for ((index, value) in numeroEmTexto.withIndex()) {
-        essevai[index] = numeroEmTexto[teste - index].digitToInt()
-        println(essevai[index])
-    }
-
-
-
-}
 object Kata {
-    fun digitize(n: Long): IntArray {
+    fun digitize(n:Long):IntArray {
         val convertingLongIntoChararray = n.toString().toCharArray()
         val arrayRealPositions = (convertingLongIntoChararray.size - 1)
-        var result = intArray(convertingLongIntoChararray.size)
+        var result = IntArray(convertingLongIntoChararray.size)
         for ((index, value) in convertingLongIntoChararray.withIndex()) {
             result[index] = convertingLongIntoChararray[arrayRealPositions - index].digitToInt()
         }
